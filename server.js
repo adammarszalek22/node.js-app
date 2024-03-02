9e5e6ee553b1e6dcefe7004ea07755cc35736328
Adam Marszalek <adammarszalek60@gmail.com>
2024-03-02 08:10:04 +0000

    .

--- server.js
+++ server.js
@@ -38,16 +38,15 @@ const tourSchema = new mongoose.Schema({
 const Tour = mongoose.model('Tour', tourSchema);
 
 const testTour = new Tour({
-	name: 'The Forest Hiker4',
-	price: 4.97,
-	rating: 4.7
+	name: 'The Forest Hiker5',
+	price: 6
 })
 
 testTour.save().then(doc => {
 	console.log(doc);
 })
 .catch(err => {
-	console.log('ERROR')
+	console.log('ERROR' + err)
 });
 
 // console.log(process.env);
